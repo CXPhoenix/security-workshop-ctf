9a8f14650fc4d3cb68da0f110cced56ebc35345e
CXPhoenix <jate9229@gmail.com>
2022-04-06 20:49:11 +0800

    feat: 🎸 new index page

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "index.html"),
         lv0: resolve(__dirname, "level0/index.html"),
-        // lv1: resolve(__dirname, "level1/index.html"),
-        // lv2: resolve(__dirname, "level2/index.html"),
-        // lv3: resolve(__dirname, "level3/index.html"),
-        // lv4: resolve(__dirname, "level4/index.html"),
+        lv1: resolve(__dirname, "level1/index.html"),
+        lv2: resolve(__dirname, "level2/index.html"),
+        lv3: resolve(__dirname, "level3/index.html"),
+        lv4: resolve(__dirname, "level4/index.html"),
       },
     },
   },
