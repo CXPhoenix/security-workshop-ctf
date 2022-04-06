9fa8774b94f493c5739065b048f2a515c1e0d2ff
CXPhoenix <jate9229@gmail.com>
2022-04-07 07:54:30 +0800

    feat: 🎸 level 3 is ready to launch

---
+++ level3/main.js
@@ -0,0 +1,80 @@
+import "../src/index.css";
+
+const demoData = [
+  {
+    message: "哈...終於做好了",
+    timestamp: new Date("2022/04/05 12:00:00"),
+  },
+  {
+    message: "差點就被 crack 了...",
+    timestamp: new Date("2022/04/06 17:28:32"),
+  },
+  {
+    message: "希望不會太糟糕😣😣😣",
+    timestamp: new Date("2022/04/07 03:24:43"),
+  },
+];
+
+const form = document.querySelector("#leave-message-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addMessageInClient(form["leave-message-area"].value, new Date());
+  demoData.push({
+    message: form["leave-message-area"].value,
+    timestamp: new Date(),
+  });
+  form["leave-message-area"].value = "";
+  window.sessionStorage.setItem("messages", JSON.stringify(demoData));
+});
+window.addEventListener("DOMContentLoaded", () => {
+  form["leave-message-area"].focus();
+  if (!window.sessionStorage.getItem("messages")) {
+    demoData.forEach((data) => {
+      addMessageInClient(data.message, data.timestamp);
+    });
+    return;
+  }
+  const messages = JSON.parse(window.sessionStorage.getItem("messages"));
+  messages.forEach((data) => {
+    addMessageInClient(data.message, data.timestamp);
+  });
+});
+
+/**
+ *
+ * @param {String} message
+ * @param {Date} timestamp
+ */
+function addMessageInClient(message, timestamp) {
+  const messageBox = document.querySelector("#message-box").content;
+  const cloneMessageBox = messageBox.cloneNode(true);
+  cloneMessageBox.querySelector("#message").innerText = message;
+  cloneMessageBox.querySelector("#timestamp").innerText = formatDate(timestamp);
+  const messages = document.querySelector("#messages");
+  messages.insertBefore(cloneMessageBox, messages.firstElementChild);
+}
+
+/**
+ *
+ * @param {Date | String} date
+ * @returns {String}
+ */
+function formatDate(date) {
+  date = new Date(date);
+  const Y = date.getFullYear();
+  const M = formatDigitalToStringStyle(date.getMonth() + 1);
+  const D = formatDigitalToStringStyle(date.getDate());
+  const h = formatDigitalToStringStyle(date.getHours());
+  const m = formatDigitalToStringStyle(date.getMinutes());
+  const s = formatDigitalToStringStyle(date.getSeconds());
+  return `${Y}-${M}-${D} ${h}:${m}:${s}`;
+}
+
+/**
+ *
+ * @param {Integer} num
+ * @returns {String}
+ */
+function formatDigitalToStringStyle(num) {
+  return num < 10 ? `0${num}` : `${num}`;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-const { resolve } = require("path");
+const { resolve, basename, extname } = require("path");
 const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
@@ -6,11 +6,27 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        lv0: resolve(__dirname, "level0/index.html"),
-        lv1: resolve(__dirname, "level1/index.html"),
-        lv2: resolve(__dirname, "level2/index.html"),
-        lv3: resolve(__dirname, "level3/index.html"),
-        lv4: resolve(__dirname, "level4/index.html"),
+        level0: resolve(__dirname, "level0/index.html"),
+        level1: resolve(__dirname, "level1/index.html"),
+        level2: resolve(__dirname, "level2/index.html"),
+        level3: resolve(__dirname, "level3/index.html"),
+        level4: resolve(__dirname, "level4/index.html"),
+      },
+      output: {
+        // assetFileNames:
+        //   "[extname]" === ".js"
+        //     ? "[name]/[name][extname]"
+        //     : "assets/[name]-[hash][extname]",
+        assetFileNames: (assetInfo) => {
+          if (extname(assetInfo.name) === ".css")
+            return "assets/[name]-[hash][extname]";
+          const file = assetInfo.name.split(__dirname).pop();
+          const absfolder = file.split(basename(file))[0];
+          const folder = absfolder.slice(1, absfolder.length - 1);
+          return `${folder.split("\\").join("/")}/[name][extname]`;
+        },
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "[name]/[name].js",
       },
     },
   },
